5214332dbd0f3d130320354f863b125fb157ec5c
xPoppa <werkmano65@gmail.com>
2022-09-02 16:04:12 +0200

    CareerCard fixed

--- components/About/Career.tsx
+++ components/About/Career.tsx
@@ -1,9 +1,11 @@
 import { Heading, VStack } from "@chakra-ui/react";
+import CareerCard from "./CareerCard";
 
 const Career = () => {
 	return (
 		<VStack>
 			<Heading>Career</Heading>
+			<CareerCard />
 		</VStack>
 	);
 };
