4637e64c2e13c0b15679377714407b75ff8a7daf
xPoppa <werkmano65@gmail.com>
2022-09-06 17:18:28 +0200

    blup

--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -3,23 +3,15 @@ import data from "../../Data/careerData";
 import ProjectCard from "./ProjectsCard";
 
 const Projects = () => {
-	const ProjectsCards = data.map((project, idx) => {
-		return (
-			<ProjectCard
-				key={idx + 20}
-				company={project.company}
-				jobTitle={project.jobTitle}
-				techStack={project.techStack}
-				date={project.date}
-			/>
-		);
+	const _ProjectsCards = data.map((project, idx) => {
+		return <ProjectCard key={idx + 20} />;
 	});
 
 	return (
 		<VStack>
 			<Heading>Projects</Heading>
 			<Text>I have not worked on Projects yet.</Text>
-			{ProjectsCards}
+			{_ProjectsCards}
 		</VStack>
 	);
 };
